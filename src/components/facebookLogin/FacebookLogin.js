d199b2d60999a1fea91432994cb40f3ab16b517a
Lillian Chernin <l.smith1654@gmail.com>
2018-04-05 12:01:24 -0500

    recreate main page

--- src/components/facebookLogin/FacebookLogin.js
+++ src/components/facebookLogin/FacebookLogin.js
@@ -6,6 +6,7 @@ class FacebookLogin extends Component {
     return (
       <div class='FacebookLogin'>
         <h1>Facebook Login</h1>
+        <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
       </div>
     )
   }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './Home.css';
+import nannyTribe from '../../images/nanny-tribe-icon.png';
+import twitter from '../../images/twitter-icon.png';
+import pinterest from '../../images/pinterest-icon.png';
+import facebook from '../../images/facebook-icon.png';
 
 class Home extends Component {
   render() {
     return (
-      <div className='Home'>
-        <h1>Home</h1>
+      <div className="Home">
+        <img className="nannyTribeMainIcon" src={nannyTribe} alt="twitter"/>
+        <div className="mainSignupButton">
+          <Link className="Link SignupLink" to={'/login'}><h3 className="mainSignupButtonText">sign up</h3></Link>
+        </div>
+        <div className="mainLoginButton">
+          <Link className="Link LoginLink" to={'/login'}><h3 className="mainLoginButtonText">login</h3></Link>
+        </div>
+        <h5 className='followHeader'>follow the leader</h5>
+        <div className='followIconsContainer'>
+          <a href="https://twitter.com/oliveyounannywi" target="_blank">
+            <img className="twitterIcon" src={twitter} width="39" alt="twitter"/>
+          </a>
+          <a href="https://www.pinterest.com/oliveyouclt/" target="_blank">
+            <img className="pinterestIcon" src={pinterest} width="39" alt="pinterest"/>
+          </a>
+          <a href="https://www.facebook.com/Olive-You-Nanny-Austin-355737071176496/" target="_blank">
+            <img className="facebookIcon" src={facebook} width="39" alt="facebook"/>
+          </a>
+        </div>
       </div>
     )
   }
